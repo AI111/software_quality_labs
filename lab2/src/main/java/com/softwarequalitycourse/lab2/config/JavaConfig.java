d3fe10833dfdbd1e7d4f891c925121d57aac04a4
sasha <sashaandreev7@gmail.com>
2015-12-12 19:42:20 +0200

    add hibernate

--- lab2/src/main/java/com/softwarequalitycourse/lab2/config/JavaConfig.java
+++ lab2/src/main/java/com/softwarequalitycourse/lab2/config/JavaConfig.java
@@ -8,12 +8,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
@@ -41,33 +37,10 @@ public class JavaConfig {
         dataSource.setPassword(env.getProperty("jdbc.password"));
         return dataSource;
     }
-//    @Bean
-//    public SessionFactory sessionFactory() {
-//        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-//        sessionFactory.setDataSource(dataSource());
-//        sessionFactory.setPackagesToScan(new String[]{"com.softwarequalitycourse.lab2.domain" });
-//        sessionFactory.setHibernateProperties(hibernateProperties());
-//        return sessionFactory.getObject();
-//    }
 
-//    @Bean
-//    public SessionFactory sessionFactory() throws Exception {
-//
-//        AnnotationSessionFactoryBean sessionFactoryBean = new AnnotationSessionFactoryBean();
-//        sessionFactoryBean.setDataSource(dataSource());
-//        sessionFactoryBean.setAnnotatedPackages("com.softwarequalitycourse.lab2");
-//        //sessionFactoryBean.setPackagesToScan(new String[]{"com.softwarequalitycourse.lab2.domain" });
-//        sessionFactoryBean.setHibernateProperties(hibernateProperties());
-//        sessionFactoryBean.afterPropertiesSet();
-//        return sessionFactoryBean.getObject();
-//    }
 
     private Properties hibernateProperties() {
         Properties properties = new Properties();
-        System.out.println("HibernateProperties");
-        System.out.println(env.getProperty("hibernate.dialect"));
-        System.out.println(env.getProperty("hibernate.show_sql"));
-        System.out.println(env.getProperty("hibernate.format_sql"));
 
         properties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
         properties.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
@@ -75,35 +48,6 @@ public class JavaConfig {
         return properties;
     }
 
-//    @Bean
-//    public HibernateTransactionManager transactionManager() {
-//        HibernateTransactionManager txManager = new HibernateTransactionManager();
-//        txManager.setSessionFactory(sessionFactory());
-//        return txManager;
-//    }
-//@Bean
-//public HibernateTransactionManager transactionManager(SessionFactory factory) {
-//    return new HibernateTransactionManager(factory);
-//}
-//@Bean
-//public SessionFactory sessionFactory() throws Exception {
-//
-//    AnnotationSessionFactoryBean sessionFactoryBean = new AnnotationSessionFactoryBean();
-//    sessionFactoryBean.setDataSource(dataSource());
-//    sessionFactoryBean.setPackagesToScan(new String[]{"com.softwarequalitycourse.lab2"});
-//
-//    Properties hibernateProperties = new Properties();
-//    hibernateProperties.put("hibernate.show_sql", true);
-//    hibernateProperties.put("persistenceUnitName", "jpaData");
-//    hibernateProperties.put("hibernate.format_sql", false);
-//    hibernateProperties.put("hibernate.hbm2ddl.auto", "update");
-//    hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
-//
-//    sessionFactoryBean.setHibernateProperties(hibernateProperties);
-//    sessionFactoryBean.afterPropertiesSet();
-//
-//    return sessionFactoryBean.getObject();
-//}
 
     @Bean
     public LocalSessionFactoryBean sessionFactory() {
@@ -113,16 +57,8 @@ public class JavaConfig {
         sessionFactory.setHibernateProperties(hibernateProperties());
         return sessionFactory;
     }
-//    @Bean
-//    public HibernateTransactionManager transactionManager() throws Exception {
-//
-//        HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager();
-//
-//        hibernateTransactionManager.setSessionFactory(sessionFactory());
-//
-//        return hibernateTransactionManager;
-//    }
-@Bean
+
+    @Bean
 @Autowired
 public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
     HibernateTransactionManager txManager = new HibernateTransactionManager();
